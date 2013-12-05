527c646fda6769869779dc0a88e2accec28c4c4b
Diego Barrio H <diegobarrioh@gmail.com>
2013-12-05 16:57:47 +0100

    casi finalizado el tratamiento de las ramas remotas, origin y demás

--- plohmygit/segment/plohmygit.py
+++ plohmygit/segment/plohmygit.py
@@ -8,20 +8,41 @@ import os
 import subprocess
 
 icon = {
+    'branch': ' ',
     'staged': ' ',
     'added':  ' ',
     'modified': ' ',
-    'modified_cached': ' ',
-    'renamed':  '☁',
+    'modified_cached': ' ',
+    'renamed':  ' ',
     'deleted':  ' ',
     'deleted_cached':  ' ',
-    'untracked': ' '
+    'untracked': ' ',
+    'tag': ' ',
+    'stashed': ' ',
+    'behind': ' ',
+    'ahead': ' ',
+    'diverged': ' ',
+    'upstream': '  ',
+    'will_merge': ' ',
+    'will_rebase': ' ',
+    'can_fast_forward': ' ',
+    'should_push': ' ',
+    'deatached_head': ' '
 }
 
 @requires_segment_info
 def plohmygit(pl, segment_info,use_path_separator=False):
 
-    branch = None
+    current_branch = ''
+    has_tag = False
+    has_stashed = False
+    has_diverged = False
+    has_upstream = False
+    can_fast_forward = False
+    deatached_head = False
+    just_init = False
+    will_rebase = False
+    will_merge = False
 
     has_modified = 0
     has_modified_cached = 0
@@ -30,19 +51,72 @@ def plohmygit(pl, segment_info,use_path_separator=False):
     has_deleted_cached = 0
     has_untracked = 0
     has_renamed = 0
+    commits_ahead = 0
+    commits_behind = 0
+
 
     ret = []
-    draw_inner_divider = not use_path_separator
+    draw_inner_divider = use_path_separator
 
-    gitsym =  Popen(['git','branch','--l'],stdout=PIPE, stderr=PIPE)
-    branch, error = gitsym.communicate()
-
-    error_string = error.decode('utf-8')
-
-    if 'fatal: Not a git repository' in error_string:
+    #first we are going to check if it's a git repo....
+    current_commit_line = Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(current_commit_line) > 0:
+        current_commit_hash = current_commit_line[0]
+    else:
+        #we are done
         return None
 
-    lineas = Popen(['git','status','--porcelain'],stdout=PIPE).communicate()[0].splitlines()
+    ''' we are in a git repo
+        Let's take a look over here...
+    '''
+    current_branch_line = Popen(['git', 'rev-parse','--abbrev-ref', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(current_branch_line) > 0:
+        current_branch = current_branch_line[0]
+
+    if current_branch == 'HEAD':
+        deatached_head = True
+
+    hay_algun_log_line = Popen(['git', 'log','--pretty=oneline', '-n1'], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(hay_algun_log_line) == 0:
+        just_init = True
+
+    upstream_line = Popen(['git', 'rev-parse', '--symbolic-full-name','--abbrev-ref','@{u}'], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(upstream_line) > 0:
+        has_upstream = True
+        upstream = upstream_line[0]
+
+    tag_at_current_commit_line = Popen(['git', 'describe', '--exact-match','--tags',current_commit_hash], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(tag_at_current_commit_line) > 0:
+        has_tag = True
+        tag_at_current_commit = tag_at_current_commit_line[0]
+
+    if has_upstream:
+        commits_describe_line = Popen(['git', 'log', '--pretty=oneline','--no-color','--topo-order','--left-right','%s%s%s' % (current_commit_hash,'...',upstream)], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+        #ahora tendría que hacer un grep para contar los commits ahead o commits behind, pero como no sé pasar la instruccion a Popen lo voy a hacer contando las lineas con startswith
+        if commits_describe_line > 0:
+            for line in commits_describe_line:
+                if line.startswith('<'):
+                    commits_ahead += 1
+                if line.startswith('>'):
+                    commits_behind += 1
+    if commits_ahead > 0 and commits_behind > 0:
+        has_diverged = True
+    if commits_ahead == 0 and commits_behind > 0:
+        can_fast_forward = True
+
+    will_rebase_line = Popen(['git', 'config', '--get','%s.%s.%s' % ('branch',current_branch,'rebase')], stdout=PIPE, stderr=PIPE).communicate()[0].splitlines()
+    if len(will_rebase_line) > 0:
+        will_rebase = will_rebase_line[0]
+
+    number_of_stashes = 0
+    #stashed files
+    stashed_lines = Popen(['git','stash','list'],stdout=PIPE,stderr=PIPE).communicate()[0].splitlines()
+    if len(stashed_lines) > 0:
+        has_stashed = True
+        for line in stashed_lines:
+            number_of_stashes += 1
+
+    lineas = Popen(['git', 'status', '--porcelain'],stdout=PIPE).communicate()[0].splitlines()
 
     for line in lineas:
         #la linea empieza por:
@@ -70,6 +144,68 @@ def plohmygit(pl, segment_info,use_path_separator=False):
         if line.startswith('??'):
             has_untracked += 1
 
+
+    dirty = has_staged + has_modified + has_modified_cached + has_renamed + has_deleted + has_deleted_cached + has_untracked
+
+    if deatached_head:
+        if just_init:
+            current_branch = 'deatached'
+        else:
+            current_branch = current_commit_hash[:7]
+        ret.append({
+            'contents': "%s%s" % (icon['deatached_head'],current_branch),
+            'highlight_group':   'deatached_head',
+            'divider_highlight_group': 'cwd:divider',
+            'draw_inner_divider': draw_inner_divider,
+            })
+
+    else:
+        ret.append({
+            'contents': "%s%s" % (icon['branch'],current_branch),
+            'highlight_group':   'branch_dirty' if dirty else 'branch_clean',
+            'divider_highlight_group': 'cwd:divider',
+            'draw_inner_divider': draw_inner_divider,
+        })
+
+    if has_upstream:
+        if will_rebase:
+            ret.append({
+                'contents': "%s%s %s" % (icon['upstream'],upstream,icon['will_rebase']),
+                'highlight_group':   'upstream',
+                'divider_highlight_group': 'cwd:divider',
+                'draw_inner_divider': draw_inner_divider,
+            })
+        else:
+            ret.append({
+                'contents': "%s%s %s" % (icon['upstream'],upstream,icon['will_merge']),
+                'highlight_group':   'upstream',
+                'divider_highlight_group': 'cwd:divider',
+                'draw_inner_divider': draw_inner_divider,
+            })
+
+        if has_diverged:
+            ret.append({
+                'contents': "-%s %s+%s" % (commits_behind,icon['diverged'],commits_ahead),
+                'highlight_group':   "diverged",
+                'divider_highlight_group': 'cwd:divider',
+                'draw_inner_divider': draw_inner_divider,
+            })
+        else:
+            if commits_behind > 0:
+                ret.append({
+                    'contents': "%s-%s" % (icon['can_fast_forward'],commits_behind),
+                    'highlight_group':   "commits:behind",
+                    'divider_highlight_group': 'cwd:divider',
+                    'draw_inner_divider': draw_inner_divider,
+                })
+            if commits_ahead > 0:
+                ret.append({
+                'contents': "%s+%s" % (icon['should_push'],commits_ahead),
+                'highlight_group':   "commits:ahead",
+                'divider_highlight_group': 'cwd:divider',
+                'draw_inner_divider': draw_inner_divider,
+            })
+
     if has_staged > 0:
         ret.append({
             'contents': "%s%s" % (icon['staged'],has_staged),
@@ -77,6 +213,7 @@ def plohmygit(pl, segment_info,use_path_separator=False):
             'divider_highlight_group': 'cwd:divider',
             'draw_inner_divider': draw_inner_divider,
         })
+
     if has_modified > 0:
         ret.append({
             'contents': "%s%s" % (icon['modified'],has_modified),
@@ -84,6 +221,8 @@ def plohmygit(pl, segment_info,use_path_separator=False):
             'divider_highlight_group': 'cwd:divider',
             'draw_inner_divider': draw_inner_divider,
         })
+
+
     if has_modified_cached > 0:
         ret.append({
             'contents': "%s%s" % (icon['modified_cached'],has_modified_cached),
@@ -119,5 +258,20 @@ def plohmygit(pl, segment_info,use_path_separator=False):
             'divider_highlight_group': 'cwd:divider',
             'draw_inner_divider': draw_inner_divider,
         })
+    if has_tag:
+        ret.append({
+            'contents': "%s%s" % (icon['tag'],tag_at_current_commit),
+            'highlight_group': "tagged",
+            'divider_highlight_group': 'cwd:divider',
+            'draw_inner_divider': draw_inner_divider,
+        })
+    if has_stashed:
+        ret.append({
+            'contents': "%s%s" % (icon['stashed'],number_of_stashes),
+            'highlight_group': "stashed",
+            'divider_highlight_group': 'cwd:divider',
+            'draw_inner_divider': draw_inner_divider,
+        })
+
     return ret
 
